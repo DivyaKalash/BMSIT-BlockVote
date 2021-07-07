c7a64b895bbfddd05d0385fe38b4287e22a58898
Aman Prasad <70457697+Striverr07@users.noreply.github.com>
2021-07-08 01:52:59 +0530

    Add files via upload

---
+++ client/src/components/Content/Content.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Cont.scss";
+const Content = () => {
+  return (
+    <div class="container">
+      <div class="card__container">
+        <div class="card">
+          <div class="card__content">
+            <h3 class="card__header">Voting</h3>
+            <p class="card__info">Voting For Placement Cordinator</p>
+            <Link to={"/Main"}>
+              <button class="card__button"> Go </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Content;
