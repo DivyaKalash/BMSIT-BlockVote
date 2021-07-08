b42f3a959794561f103b5fa0f9cb3fb11f79b569
Aman Prasad <70457697+Striverr07@users.noreply.github.com>
2021-07-08 08:41:10 +0530

    Update Content.js

--- client/src/components/Content/Content.js
+++ client/src/components/Content/Content.js
@@ -9,7 +9,7 @@ const Content = () => {
           <div class="card__content">
             <h3 class="card__header">Voting</h3>
             <p class="card__info">Voting For Placement Cordinator</p>
-            <Link to={"/Main"}>
+            <Link to={"/Dash"}>
               <button class="card__button"> Go </button>
             </Link>
           </div>
