8a974268d8b39b8f6d0c4007ec29b6316eeaaadc
DivyaKalash <67993272+DivyaKalash@users.noreply.github.com>
2021-07-09 12:02:40 +0530

    Delete client directory

--- client/README.md
+++
@@ -1,1 +0,0 @@
-# BMSIT-BlockVote
--- client/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import rootReducer from "../reducers";
-import thunk  from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
-export default store;
